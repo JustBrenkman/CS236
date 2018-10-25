9a0a65cc18bc2de45015cf87f4bf88ccd290afee
JustBrenkman <JustBrenkman@gmail.com>
2018-10-25 15:49:58 -0600

    Added function to change name of relation, this will help keep track of databases and the effects of modifications

--- database/main.cpp
+++ database/main.cpp
@@ -10,7 +10,7 @@ int main(int argc, char **argv) {
     auto relation = new Relation();
     relation->addColumns("A", "B", "C", "D");
     relation->insertRows({{"Hello", "Hello", "Hello", "Hello"},
-                          {"Hello", "list",  "That"}});
+                          {"Hello", "list",  "That",  "Super long string just for fun"}});
     relation->setName("R");
 
     auto second = relation->select(std::make_pair("A", "Hello"), std::make_pair("B", "list"));
